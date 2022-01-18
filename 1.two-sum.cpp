78645acfa54d01c41cf41fd51a89c100c95b09a6
Harry <xiaohanwu12@gmail.com>
2022-01-17 20:37:24 -0500

    Update a new method

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -8,22 +8,34 @@
 class Solution {
 public:
 // brute force
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++)
-        {
-            for (int j = i+1; j < nums.size(); j++)
-            {
-                if (nums[i]+nums[j]==target)
-                {
-                    return vector<int > {i, j};
-                }
+    // vector<int> twoSum(vector<int>& nums, int target) {
+    //     for (int i = 0; i < nums.size(); i++)
+    //     {
+    //         for (int j = i+1; j < nums.size(); j++)
+    //         {
+    //             if (nums[i]+nums[j]==target)
+    //             {
+    //                 return vector<int > {i, j};
+    //             }
                 
-            }
-        }
-        return vector<int> {0, 0};
+    //         }
+    //     }
+    //     return vector<int> {0, 0};
         
+    // }
+
+    // use map to store and find
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> umap;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            int diff = target-nums[i];
+            if(umap.count(diff) > 0) return vector<int>({umap[diff], i});
+            umap[nums[i]] = i;
+        }
+        return vector<int>();
     }
-// TODO: use map to store and find
+
 };
 // @lc code=end
 
